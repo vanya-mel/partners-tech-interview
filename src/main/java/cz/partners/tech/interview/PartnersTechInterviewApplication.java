dc41449334c8a872537b0d6524d8f51c80dae104
Vanya Melnykovych <vanya.melnykovych@gmail.com>
2025-10-16 07:30:40 +0200

    Init base project with needed dependencies

---
+++ src/main/java/cz/partners/tech/interview/PartnersTechInterviewApplication.java
@@ -0,0 +1,13 @@
+package cz.partners.tech.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnersTechInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartnersTechInterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/partners/tech/interview/PartnersTechInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.partners.tech.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnersTechInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
